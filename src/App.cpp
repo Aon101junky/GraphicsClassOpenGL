148e820568781802c82c92c0e2067d291ed08ce4
Aon101kunky <falloutaaron@gmail.com>
2022-02-16 12:56:37 -0600

    Drew a triangle

--- src/App.cpp
+++ src/App.cpp
@@ -51,6 +51,62 @@ void App::Load()
 
   }
 
+  //Fragment Shader
+  unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  //Check for compile error
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if(!success)
+  {
+
+  glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "Error::Shader::FRAGMENT::CompileFailure\n" << infoLog << std::endl;
+
+  }
+
+  //Link shaders
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  //Check for link errors
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if(!success)
+  {
+
+  glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "Error::Shader::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  float vertices[] = {
+    -0.5f, -0.5f, 0.0f, //left
+    0.5f, -0.5f, 0.0f, //right
+    0.0f, 0.5f, 0.0f //top
+  };
+
+  //VBO, VAO
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
+
 }
 void App::Loop()
 {
@@ -66,7 +122,20 @@ void App::Loop()
   }
 }
 void App::Update(){}
-void App::Draw(){}
+void App::Draw()
+{
+
+  glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  //Draw first triangle
+
+  glUseProgram(shaderProgram);
+  glBindVertexArray(VAO);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glBindVertexArray(0);
+
+}
 void App::LateUpdate(){}
 void App::FixedUpdate(float _delta_time){}
 void App::InputUpdate()
