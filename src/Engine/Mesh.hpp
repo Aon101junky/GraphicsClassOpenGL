314635dfad337600fd6e53b8f217da101f382fbe
Aon101kunky <falloutaaron@gmail.com>
2022-04-13 12:58:36 -0500

    End of class 4/13/2022

---
+++ src/Engine/Mesh.hpp
@@ -0,0 +1,32 @@
+#include <gl/glew.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <string>
+#include <vector>
+
+#include "Shader.hpp"
+
+#define MAX_BONE_INFLUENCE 4
+
+namespace Engine
+{
+    struct Vertex
+    {
+        glm::vec3 Position;
+        glm::vec3 Normal;
+        glm::vec2 TexCoords;
+        glm::vec3 Tangent;
+        glm::vec3 Bitangent;
+        int m_BoneIDs[MAX_BONE_INFLUENCE];
+        float m_Weights[MAX_BONE_INFLUENCE];
+    }
+
+    struct Mesh_Texture
+    {
+        unsigned int id;
+        std::string type;
+        std::string path;
+    }
+}
